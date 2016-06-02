8b909729501b33b3e81051f7431dc2cc8bd5bba3
YChrisHu <13901860r@connect.polyu.hk>
2016-06-02 22:50:58 +0800

    classic one, waiting to modify

---
+++ Torch/cnn_model.lua
@@ -0,0 +1,29 @@
+require 'cunn'
+
+
+function cnn_model()
+   local model = nn.Sequential() 
+   
+   -- convolution layers
+   model:add(nn.SpatialConvolutionMM(1, 1, 5, 5, 1, 1))
+   model:add(nn.ReLU())
+--   model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+   
+   model:add(nn.SpatialConvolutionMM(1, 2, 5, 5, 1, 1))
+   model:add(nn.ReLU())
+--   model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+   
+   model:add(nn.SpatialConvolutionMM(256, 512, 4, 4, 1, 1))
+   model:add(nn.ReLU())
+   
+   -- fully connected layers
+   model:add(nn.SpatialConvolutionMM(512, 1024, 2, 2, 1, 1))
+   model:add(nn.ReLU())
+--   model:add(nn.Dropout(0.5))
+   model:add(nn.SpatialConvolutionMM(1024, 10, 1, 1, 1, 1))
+   
+   model:add(nn.Reshape(10))
+   model:add(nn.SoftMax())
+   
+   return model
+end
