3925c60bf725fbcd2081435f05c5b5b8a8e914a0
YChrisHu <13901860r@connect.polyu.hk>
2016-06-10 03:18:20 +0800

    V0.1.2.19

---
+++ Torch/cnn_data.lua
@@ -0,0 +1,22 @@
+----------------------------------------------------------------------
+--
+--
+--
+--
+--
+--
+--
+----------------------------------------------------------------------
+
+local matio = require 'matio'
+
+-- load a single array from file
+traindata.data = matio.load('traindata.mat', 'train_x')
+traindata.label = matio.load('traindata.mat', 'train_y')
+testdata.data = matio.load('testdata.mat', 'train_x')
+testdata.label = matio.load('testdata.mat', 'train_y')
+validdata.data = matio.load('validdata.mat', 'valid_x')
+validdata.label = matio.load('validdata.mat', 'valid_y')
+
+-- load all arrays from file
+--tensors = matio.load('test.mat')
