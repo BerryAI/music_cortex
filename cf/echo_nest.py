3f727035d53fe2d5c9138d9fb4c29936de3b0c71
wangziteng <kronos1980.wang@gmail.com>
2016-07-11 00:03:15 -0500

    Adding Gradient Descent Method (#4)
    * Adding Gradient Descent Method
    
    * Update stochastic methods

--- cf/echo_nest.py
+++ cf/echo_nest.py
@@ -1,11 +1,12 @@
 import io
 import operator
 import numpy
-from hidden_feature_prediction import full_rating_matrix_with_index
+from hidden_feature_prediction import *
 
 def get_song_ID_index(filename):
 
     song_index = dict()
+    name_index = dict()
 
     with io.open(filename,'r') as fp:
         count = 0
@@ -14,9 +15,12 @@ def get_song_ID_index(filename):
             track_ID = contents[1]
             if track_ID not in song_index:
                 song_index[track_ID] = count
+                if count not in name_index:
+                    info = contents[2] + "<SEP>" + contents[3]
+                    name_index[count] = info
                 count += 1
 
-    return song_index
+    return song_index, name_index
 
 def get_echo_nest_user_history(filename, song_index):
 
@@ -65,7 +69,7 @@ def get_hidden_feature_matrix(filename_echo_nest, filename_tracks, k):
     :rtype: ndarray
     """
 
-    song_index = get_song_ID_index(filename_tracks)
+    song_index, name_index = get_song_ID_index(filename_tracks)
     echo_nest_user_history = get_echo_nest_user_history(filename_echo_nest, song_index)
     user_rating_dict = get_user_rating_from_history_echo_nest(echo_nest_user_history, song_index)
 
@@ -88,12 +92,59 @@ def get_hidden_feature_matrix(filename_echo_nest, filename_tracks, k):
 
     return V_bar.T
 
+def write_song_index_file(song_index_rate, name_index, index_filename):
+
+    inv_song_index_rate = {v: k for k, v in song_index_rate.items()}
+    with io.open(index_filename,'w',encoding='utf8') as f:
+        for key in inv_song_index_rate:
+            info = str(key) + "<SEP>" + name_index[inv_song_index_rate[key]] + "\n"
+            f.write(info)
+
+
+def get_hidden_feature_matrix_SGD(filename_echo_nest, filename_tracks, k, lean_rate, lambda_rate, max_iter):
+    """Get hidden feature matrix by stochastic gradient descent method
+
+    :param filename: filename of unique MSD tracks
+    :param tracks_filename: filename of all unique tracks
+    :param base_dir: base directory of data
+    :return data: hidden feature dataset
+    :rtype: ndarray
+    """
+
+    song_index, name_index = get_song_ID_index(filename_tracks)
+    echo_nest_user_history = get_echo_nest_user_history(filename_echo_nest, song_index)
+    user_rating_dict = get_user_rating_from_history_echo_nest(echo_nest_user_history, song_index)
+
+    user_rating_length = dict()
+    for user in user_rating_dict:
+        user_rating_length[user] = len(user_rating_dict[user])
+
+    sorted_user_rating_length = sorted(user_rating_length.items(), key=operator.itemgetter(1), reverse=True)
+
+    top_user_rating_dict = dict()
+    for value in sorted_user_rating_length[0:1000]:
+        top_user_rating_dict[value[0]] = user_rating_dict[value[0]]
+    user_index, song_index_rate, rating_matrix = full_rating_matrix_with_index(top_user_rating_dict)
+
+    write_song_index_file(song_index_rate, name_index, "index_file.txt")
+
+    print "SGD starts"
+
+    user_weight, hidden_feature = stochastic_GD(rating_matrix, lean_rate, lambda_rate, k, max_iter)
+
+    return user_weight, hidden_feature
+
+k = 5
+lean_rate = 0.00001
+lambda_rate = 0.00
+max_iter = 5000
 
 filename_tracks = "subset_unique_tracks.txt"
 filename_echo_nest = "train_triplets.txt"
 k = 5
-hidden_feature_matrix = get_hidden_feature_matrix(filename_echo_nest, filename_tracks, k)
+user, hidden_feature_matrix = get_hidden_feature_matrix_SGD(filename_echo_nest, filename_tracks, k, lean_rate, lambda_rate, max_iter)
 
+print hidden_feature_matrix[0:10,:]
 hist, bin_edges = numpy.histogram(hidden_feature_matrix, bins=20)
 print hist
 print bin_edges
