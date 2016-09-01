e42ca6145f70a7018d7231f41e84d45095e91273
ZZ <zhangzhang.si@gmail.com>
2016-08-31 23:07:36 -0500

    fix packaging issue

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='OpenMRS',
       version='0.0.1',
       description='Open source music recommendation.',
-      packages=['OpenMRS'],
+      packages=find_packages('.'),
+      include_package_data=True,
+      #data_files=[('OpenMRS/data', ['OpenMRS/data/acai_game_user_ratings.json'])],
       zip_safe=False)
